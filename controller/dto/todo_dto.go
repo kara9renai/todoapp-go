d9a01fbf8cb4f82ee586e791dc0f4ab90bb9f1f1
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-03-18 23:49:42 +0900

    part1の答えを実装

--- controller/dto/todo_dto.go
+++ controller/dto/todo_dto.go
@@ -1,10 +1,16 @@
 package dto
 
 type TodoResponse struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 type TodoRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 type TodosResponse struct {
+	Todos []TodoResponse `json:"todos"`
 }
--- model/entity/todo_entity.go
+++ model/entity/todo_entity.go
@@ -2,4 +2,7 @@ package entity
 
 type TodoEntity struct {
 	// 構造体を定義する
+	ID      int
+	Title   string
+	Content string
 }
