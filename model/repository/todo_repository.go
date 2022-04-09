0097be9fa7896098e9299e84e01ac8ad6527e546
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-04-09 14:10:46 +0900

    Repository層の実装

--- model/repository/todo_repository.go
+++ model/repository/todo_repository.go
@@ -1,11 +1,14 @@
 package repository
 
 import (
+	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/kara9renai/todoapp-go/model/entity"
 )
 
 type TodoRepository interface {
+	GetTodos() (todos []entity.TodoEntity, err error)
 }
 
 type todoRepository struct {
@@ -16,6 +19,23 @@ func NewTodoRepository() TodoRepository {
 }
 
 func (tr *todoRepository) GetTodos() (todos []entity.TodoEntity, err error) {
+	const query = `SELECT id, title, content FROM todo ORDER BY id DESC`
+	todos = []entity.TodoEntity{}
+	// クエリの実行
+	rows, err := Db.Query(query)
+	if err != nil {
+		log.Print(err)
+	}
+
+	for rows.Next() {
+		todo := entity.TodoEntity{}
+		err = rows.Scan(&todo.ID, &todo.Title, &todo.Content)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		todos = append(todos, todo)
+	}
 	return
 }
 
