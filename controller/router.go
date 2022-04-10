9601528b8f296a03b92c3cfbfbcfe776258bc4c6
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-04-10 15:36:24 +0900

    routingを設定する

--- controller/router.go
+++ controller/router.go
@@ -18,6 +18,8 @@ func (ro *router) HandleTodosRequest(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		ro.tc.GetTodos(w, r)
+	case "POST":
+		ro.tc.PostTodo(w, r)
 	default:
 		w.WriteHeader(405)
 	}
