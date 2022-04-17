70e7afd010646aab17f9bb3f00c3be0c5d779524
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-04-17 21:25:35 +0900

    クエリを追加

--- model/repository/todo_repository.go
+++ model/repository/todo_repository.go
@@ -16,17 +16,29 @@ func NewTodoRepository() TodoRepository {
 }
 
 func (tr *todoRepository) GetTodos() (todos []entity.TodoEntity, err error) {
+	//以下のクエリを使用してください
+	//lint:ignore U1000 It's ok because this is a sample query.
+	const query = `SELECT id, title, content FROM todo ORDER BY id DESC`
 	return
 }
 
 func (tr *todoRepository) InsertTodo(todo entity.TodoEntity) (id int, err error) {
+	//以下のクエリを使用してください
+	//lint:ignore U1000 It's ok because this is a sample query.
+	const insert = `INSERT INTO todo (title, content) VALUES (?, ?)`
 	return
 }
 
 func (tr *todoRepository) UpdateTodo(todo entity.TodoEntity) (err error) {
+	//以下のクエリを使用してください
+	//lint:ignore U1000 It's ok because this is a sample query.
+	const update = `UPDATE todo SET title = ?, content = ? WHERE id = ?`
 	return
 }
 
 func (tr *todoRepository) DeleteTodo(id int) (err error) {
+	//以下のクエリを使用してください
+	//lint:ignore U1000 It's ok because this is a sample query.
+	const deleteFmt = `DELETE FROM todo WHERE id = ?`
 	return
 }
